ab87dbbe9da3d8e0a599206ae28e5ac8189c39fd
loodnn <loudjane@163.com>
2021-06-11 16:07:36 +0800

    初步搭nav的架构，css reset啥的

--- react-accountting-tsversion/src/App.tsx
+++ react-accountting-tsversion/src/App.tsx
@@ -1,25 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div`
+height:100vh;
+display: flex;
+flex-direction: column;
+`
+
+const Main = styled.div`
+flex-grow:1;
+overflow:auto;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>   
+      <Wrapper>
+      <Main>
+      <Switch>
+          <Route path="/label">
+            <Label />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route exact path="/">
+          <Redirect to="/money"/>
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        </Main>
+      <Nav />
+      </Wrapper>
+    </Router>
+  );
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Label() {
+  return <h2>Label</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function NoMatch() {
+  return (
+    <div>"输错地址啦"</div>
   );
 }
 
